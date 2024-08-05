ce1b8902453db908bf73bdb9ed3d4437edb91257
Ayako-Ondiege <james.ouma@student.moringaschool.com>
2024-08-05 13:02:42 +0300

    firt commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import Header from "./Header";
+import About from "./About";
 import blogData from "../data/blog";
+import ArticleList from "./ArticleList"
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
+      <Header name = {blogData.name}/>
+      <About image = {blogData.image} about = {blogData.about} />
+      <ArticleList posts = {blogData.posts}/>
+      <p>
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      </p>
     </div>
   );
 }
